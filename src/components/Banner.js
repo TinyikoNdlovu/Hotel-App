1be14fe2af7b34cdd27a5bd89dd03f4d549f753e
TinyikoNdlovu <tinyiko13@yahoo.com>
2022-10-19 06:43:53 +0200

    dates ui

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./Banner.css";
 import { Button } from '@mui/material';
 
+let vector = require("../assets/vector.png");
+let adult = require("../assets/adult.png");
+let child = require("../assets/child.png");
+let arrow= require("../assets/downfilledtriangulararrow.png");
+
 function Banner() {
 
     return(
@@ -15,12 +20,33 @@ function Banner() {
         </div>
         <div className="booking-div">
             <div className="booking">
-                <div>
+                <div style={{display: "flex"}}>
                     <div className="banner-main-div">
                         <div className="booking-header">
+                            <img className="booking-vector" alt="" src={vector} />
                             <h4>Arrival Date</h4>
+                            <img className="booking-vector2" alt="" src={arrow} />
+                        </div>
+
+                        <div className="booking-bof">
+                           <h2>date</h2>
+                           <h4>date</h4>
                         </div>
                     </div>
+
+                    <div className="banner-main-div">
+                        <div className="booking-header">
+                            <img className="booking-vector" alt="" src={vector} />
+                            <h4>Check Out</h4>
+                            <img className="booking-vector2" alt="" src={arrow} />
+                        </div>
+
+                        <div className="booking-bof">
+                           <h2>date</h2>
+                           <h4>date</h4>
+                        </div>
+                    </div>
+                    
                     
                 </div>
             </div>
